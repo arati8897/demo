1ac66722ec6baabe9d9662360270d7e254ede740
aarati8897 <aratitawari2@gmail.com>
2020-07-22 00:00:59 -1200

    style applied

---
+++ src/Tailwind/tailwind.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  purge: [],
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      xxl: '1590px',
+    },
+    fontSize: {
+           'xs': '.75rem',
+           'sm': '.875rem',
+           'tiny': '.875rem',
+            'base': '1rem',
+            'lg': '1.125rem',
+            'xl': '1.25rem',
+            '2xl': '1.5rem',
+           '3xl': '1.875rem',
+           '4xl': '2.25rem',
+            '5xl': '3rem',
+            '6xl': '4rem',
+           '7xl': '6rem',
+           '8xl': '8rem',
+           '9xl': '9rem',
+          }
+    
+},
+
+
+    extend: {
+
+      fontFamily: {
+      
+           'gothom': ['Gothom Pro', ]
+            },
+
+           
+
+            
+          
+  variants: {},
+  plugins: [],
+          }}
